8faef143a0b2535eb430bde9a34f48eeda571de2
hannie <shyhannie12@gmail.com>
2022-06-05 18:40:42 +0900

    Third Commit

--- app/src/main/java/com/example/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/androidpractice/MainActivity.java
@@ -3,12 +3,95 @@ package com.example.androidpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int cnt=0;
+    int t1, t2, t3;
+    EditText v1,v2,v3;
+    Button bt_game;
+    TextView tv_res;
+
+    String resultStr="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_game = findViewById(R.id.bt_game);
+        tv_res = findViewById(R.id.tv_res);
+
+        // 랜덤 숫자 3개 생성하기
+        // Math.random()은 0이상 1미만 사이의 수를 출력한다.
+        int value1 = (int)((Math.random()*9)+1);
+        int value2 = (int)((Math.random()*9)+1);
+        int value3 = (int)((Math.random()*9)+1);
+
+        while (value2==value1)
+            value2 = (int)((Math.random()*9)+1);
+
+        while (value3==value1 || value3==value2)
+            value3 = (int)((Math.random()*9)+1);
+
+        int fv1 = value1;
+        int fv2 = value2;
+        int fv3 = value3;
+
+        bt_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v1 = findViewById(R.id.v1);
+                v2 = findViewById(R.id.v2);
+                v3 = findViewById(R.id.v3);
+
+                t1 = Integer.parseInt(v1.getText().toString());
+                t2 = Integer.parseInt(v2.getText().toString());
+                t3 = Integer.parseInt(v3.getText().toString());
+
+                int strike=0;
+                int ball=0;
+
+                if(t1==fv1)
+                    strike++;
+                else if(t1== fv2)
+                    ball++;
+                else if(t1== fv3)
+                    ball++;
+
+                if(t2==fv2)
+                    strike++;
+                else if(t2==fv1)
+                    ball++;
+                else if(t2==fv1)
+                    ball++;
+
+                if(t3==fv3)
+                    strike++;
+                else if(t3==fv1)
+                    ball++;
+                else if(t3==fv2)
+                    ball++;
+
+
+                if(strike==3)
+                    tv_res.append("You Win! \n");
+                else
+                    tv_res.append((cnt+1)+"번째 시도: "+"("+t1+", "+t2+", "+t3+") "+strike+"S "+ball+"B \n");
+                cnt++;
+
+                if(cnt==10)
+                    tv_res.append("You lose! Please try in 10 Chances");
+
+            }
+        });
+
+        int cnt = 0;
     }
+
 }
--- app/src/main/java/com/example/androidpractice/MoveActivity.java
+++ app/src/main/java/com/example/androidpractice/MoveActivity.java
@@ -3,12 +3,62 @@ package com.example.androidpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MoveActivity extends AppCompatActivity {
+
+    LinearLayout ll_move;
+    ImageView iv_move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_move);
+
+        // View 연결
+        ll_move = findViewById(R.id.ll_move);
+        iv_move = findViewById(R.id.iv_move);
+
+        // Step 1. 화면 전체에 터치 리스너 먹이기
+//        ll_move.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()){
+//                    case MotionEvent.ACTION_DOWN: // 터치 시작
+//                    case MotionEvent.ACTION_MOVE: // 드래그
+//                    case MotionEvent.ACTION_UP: // 터치 떼기
+//                        // 좌표 지정
+//                        iv_move.setX(event.getX());
+//                        iv_move.setY(event.getY());
+//                }
+//                return true;
+//            }
+//        });
+
+        iv_move.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN: // 터치 시작
+                        // v.getX,Y() : ImageView가 화면 안에 있는 좌표=>이미지의 왼쪽 꼭짓점의 좌표임.
+                        // event.getX,Y() : Imageview 안에서 마우스가 클릭하는 좌표
+                        Toast.makeText(getApplicationContext(),
+                                event.getX()+"/"+v.getX()
+                                        +"\n"+event.getY()+"/"+v.getY(),
+                                Toast.LENGTH_SHORT).show();
+                    case MotionEvent.ACTION_MOVE: // 드래그
+                    case MotionEvent.ACTION_UP: // 터치 떼기
+                        iv_move.setX(v.getX()+event.getX()-v.getWidth()/2); // 그림 정가운데 잡아서 끌기
+                        iv_move.setY(v.getY()+event.getY()-v.getHeight()/2);
+                }
+
+                return true;
+            }
+        });
+
     }
 }
